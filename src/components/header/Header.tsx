61dd36949bfeed2ee99c0b6defe628647a23b283
zh33nbekov <rai.zheenbekov@gmail.com>
2024-12-22 22:16:05 +0600

    refactor header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,26 +4,35 @@ import styles from './header.module.css';
 const Header = () => {
 	return (
 		<header className={styles.header}>
-			<h4>Git & GitHub Practice</h4>
-			<nav className={styles.header__nav}>
-				<ul className={styles.nav__list}>
-					<li className={styles.nav__item}>
-						<Link href='/' className={styles.header__link}>
-							Home
-						</Link>
-					</li>
-					<li className={styles.nav__item}>
-						<Link href='/services' className={styles.header__link}>
-							Our Services
-						</Link>
-					</li>
-					<li className={styles.nav__item}>
-						<Link href='/admin' className={styles.header__link}>
-							Admin Panel
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			<div className='wrapper'>
+				<div className={styles.header__wrapper}>
+					<h4 className={styles.header__logo}>Git & GitHub Practice</h4>
+					<div className={styles.header__actions}>
+						<nav className={styles.actions__nav}>
+							<ul className={styles.nav__list}>
+								<li className={styles.nav__item}>
+									<Link href='/' className={styles.nav__link}>
+										Home
+									</Link>
+								</li>
+								<li className={styles.nav__item}>
+									<Link href='/services' className={styles.nav__link}>
+										Our Services
+									</Link>
+								</li>
+								<li className={styles.nav__item}>
+									<Link href='/admin' className={styles.nav__link}>
+										Admin Panel
+									</Link>
+								</li>
+							</ul>
+						</nav>
+						<button className={styles.actions__button}>
+							Contact Us
+						</button>
+					</div>
+				</div>
+			</div>
 		</header>
 	);
 };
